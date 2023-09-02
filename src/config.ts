fcd16ace7f0d98ade25a4d6e7923a453a3ee577c
Ahmed Rowaihi <ahmedrowaihi1@gmail.com>
2023-09-03 02:55:49 +0300

    🐛 FIX: WEBSITE DETAILS

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = "Abdullah Aws | Personal Portfolio";
+export const SITE_DESCRIPTION =
+  "Abdullah Aws is a Fresh graduate of computer science and seeking a data analyst engineer or related field to apply my analytical and statistical skills to extract hidden ideas and patterns from data and turn them into valuable information for making intelligent decisions";
